8df660b994fccea10cde32afb287aada73a49c41
guler <gule0153@stud.kea.dk>
2021-09-24 12:04:27 +0200

    added updates

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,4 +1,11 @@
 public class Adventure {
-    // this is an update
-    //hej
+    public static void main(String[] args) {
+        Room room1 = new Room("room1", "empty");
+        Room room2 = new Room("room2", "empty");
+        Room currentRoom = room1;
+        room1.setEast(room2);
+        System.out.println(currentRoom.getName());
+        currentRoom = room2;
+        System.out.println(currentRoom.getName());
+    }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,79 @@
 public class Room {
+    private Room north;
+    private Room south;
+    private Room west;
+    private Room east;
+    private String name;
+    private String description;
+
+    public Room(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setNorth(Room north) {
+        if(this.north == null){
+            this.north = north;
+            north.setSouth(this);
+        }
+
+
+    }
+
+    public void setSouth(Room south) {
+        if(this.south == null){
+            this.south = south;
+            south.setNorth(this);
+        }
+    }
+
+    public void setWest(Room west) {
+        if(this.west == null){
+            this.west = west;
+            west.setEast(this);
+        }
+    }
+
+    public void setEast(Room east) {
+        if(this.east == null){
+            this.east = east;
+            east.setWest(this);
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean check(){
+        return false;
+    }
 }
