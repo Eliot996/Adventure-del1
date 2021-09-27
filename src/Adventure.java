2077d5d40fb12c3c359541768c64fb69601b7660
Eliot996 <math10t6@stud.kea.dk>
2021-09-27 11:06:15 +0200

    Moved map into map class

--- src/Adventure.java
+++ src/Adventure.java
@@ -2,52 +2,18 @@ import java.util.Scanner;
 
 public class Adventure {
 
-    private Room[] map;
-    private Room currentRoom;
+    private Map mapOfGame;
+    private Player player;
     Scanner input = new Scanner(System.in);
 
     public Adventure(){
-        buildMap();
-    }
-
-    public void buildMap(){
-        //Creating the room objects and array
-        map = new Room[9];
-        map[0] = new Room("Room1", "empty");
-        map[1] = new Room("Room2", "empty");
-        map[2] = new Room("Room3", "empty");
-        map[3] = new Room("Room4", "empty");
-        map[4] = new Room("Room5", "empty");
-        map[5] = new Room("Room6", "empty");
-        map[6] = new Room("Room7", "empty");
-        map[7] = new Room("Room8", "empty");
-        map[8] = new Room("Room9", "empty");
-
-        //Connected the rooms.
-        //Connected room1 with room2 and room4
-        map[0].setEast(map[1]);
-        map[0].setSouth(map[3]);
-
-        //Connected room2 with room3
-        //Connected room3 with room6
-        map[1].setEast(map[2]);
-        map[2].setSouth(map[5]);
-
-        //Connected room6 with room9
-        //Connected room7 with room4
-        map[5].setSouth(map[8]);
-        map[6].setNorth(map[3]);
-
-        //Connected room8 with room7
-        //Connected room8 with room5
-        //Connected room9 with room8
-        map[7].setWest(map[6]);
-        map[7].setNorth(map[4]);
-        map[8].setWest(map[7]);
+        mapOfGame = new Map();
+        player = new Player();
+        player.setCurrentRoom(mapOfGame.getMap()[0]);
     }
 
     public void play(){
-        currentRoom = map[0];
+        System.out.println(player.getCurrentRoom().getName());
 
         boolean gameActive = true;
         System.out.println("Welcome to the adventure game!");
@@ -77,7 +43,7 @@ public class Adventure {
     }
 
     public String look(){
-        return currentRoom.getDescription();
+        return player.getCurrentRoom().getDescription();
     }
 
     public String helpPlayer(){ // Could be done better, but for right now, this works OK.
@@ -94,21 +60,21 @@ public class Adventure {
     }
 
     public String goTo(String userInput){
-        if (userInput.equals("north") && currentRoom.hasNorth()){
-            currentRoom = currentRoom.getNorth();
-            return currentRoom.getDescription();
+        if (userInput.equals("north") && player.getCurrentRoom().hasNorth()){
+            player.setCurrentRoom(player.getCurrentRoom().getNorth());
+            return player.getCurrentRoom().getDescription();
         }
-        if(userInput.equals("south") && currentRoom.hasSouth()){
-            currentRoom = currentRoom.getSouth();
-            return currentRoom.getDescription();
+        if(userInput.equals("south") && player.getCurrentRoom().hasSouth()){
+            player.setCurrentRoom(player.getCurrentRoom().getSouth());
+            return player.getCurrentRoom().getDescription();
         }
-        if(userInput.equals("east") && currentRoom.hasEast()){
-            currentRoom = currentRoom.getEast();
-            return currentRoom.getDescription();
+        if(userInput.equals("east") && player.getCurrentRoom().hasEast()){
+            player.setCurrentRoom(player.getCurrentRoom().getEast());
+            return player.getCurrentRoom().getDescription();
         }
-        if(userInput.equals("west") && currentRoom.hasWest()){
-            currentRoom = currentRoom.getWest();
-            return currentRoom.getDescription();
+        if(userInput.equals("west") && player.getCurrentRoom().hasWest()){
+            player.setCurrentRoom(player.getCurrentRoom().getWest());
+            return player.getCurrentRoom().getDescription();
         }
         return "You cannot go that direction in this room";
     }
--- src/Map.java
+++ src/Map.java
@@ -1,7 +1,46 @@
 public class Map {
     //Creation of the rooms
+    public Room[] map;
 
     public Map(){
+        //Creating the array
+        map = new Room[9];
 
+        //Creating the room objects
+        map[0] = new Room("Room1", "empty");
+        map[1] = new Room("Room2", "empty");
+        map[2] = new Room("Room3", "empty");
+        map[3] = new Room("Room4", "empty");
+        map[4] = new Room("Room5", "treasure room");
+        map[5] = new Room("Room6", "empty");
+        map[6] = new Room("Room7", "empty");
+        map[7] = new Room("Room8", "empty");
+        map[8] = new Room("Room9", "empty");
+
+        //Connected the rooms.
+        //Connected room1 with room2 and room4
+        map[0].setEast(map[1]);
+        map[0].setSouth(map[3]);
+
+        //Connected room2 with room3
+        //Connected room3 with room6
+        map[1].setEast(map[2]);
+        map[2].setSouth(map[5]);
+
+        //Connected room6 with room9
+        //Connected room7 with room4
+        map[5].setSouth(map[8]);
+        map[6].setNorth(map[3]);
+
+        //Connected room8 with room7
+        //Connected room8 with room5
+        //Connected room9 with room8
+        map[7].setWest(map[6]);
+        map[7].setNorth(map[4]);
+        map[8].setWest(map[7]);
+    }
+
+    public Room[] getMap() {
+        return map;
     }
 }
